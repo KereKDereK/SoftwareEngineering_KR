52d95dfc509042b30f8782c2852c2f756eba1a40
KereK DereK <kerekderek1@gmail.com>
2023-03-21 18:13:52 +0400

    ConnectionObserver and ConnectionPair class  structure added

---
+++ ConnectionPair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eng1
+{
+    public class ConnectionPair
+    {
+        public int Id { get; set; }
+        public string FirstClient { get; set; }
+        public string SecondClient { get; set; }
+        public List<DateTime> ConnectionWindows { get; set; } 
+        public ConnectionPair(int id, string fclnt, string sclnt, List<DateTime> windows)
+        {
+            Id = id;
+            FirstClient = fclnt;
+            SecondClient = sclnt;
+            ConnectionWindows = windows;
+        }
+    }
+
+}
---
+++ Schedule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eng1
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public List<ConnectionPair> ConnectionPairs { get; set; }
+        private DateTime EndTime { get; set; }
+
+        Schedule(int id, string filepath)
+        {
+            Id = id;
+            ScheduleParseFromJson(filepath);
+        }
+        
+        private void ScheduleParseFromJson(string filepath)
+        {
+            //парсим все
+        }
+    }
+}
